d9b809bfc186773660d1a76e89c5fbc4ef57c47c
Harry Baines <harryb0905@googlemail.com>
2025-06-27 12:36:12 +0100

    05: ai jobs

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,26 @@
+import { openai, createAgent } from "@inngest/agent-kit";
+
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
+  async ({ event }) => {
+    // e.g. transcript step
+    // await step.sleep("wait-a-moment", "5s");
+
+    // Create a new agent with a system prompt (you can add optional tools, too)
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system:
+        "You are an expert Next.js developer. You write readable, maintainable code. You write simple Next.js & React snippets.",
+      model: openai({ model: "gpt-4o" }),
+    });
+
+    const { output } = await codeAgent.run(
+      `Write the following snippet: ${event.data.value}`,
+    );
+
+    return { output };
   },
 );
